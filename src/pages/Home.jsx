278da07e3c28bd7f07aeae0599b7e1d631db7e58
waihlyanaung <hlyan8834@gmail.com>
2023-10-17 05:00:59 +0630

    v3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,119 +70,119 @@ const Home = () => {
     }
   };
 
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      {/* for displaying animate from lottie */}
-      <img src={icon} className="sm:w-[10%] w-[45%] " />
-      <h1 className="text-3xl font-bold mb-4">Password Generator</h1>
+  const CheckboxOption = ({ label, checked, onChange }) => (
+    <div className="mb-4 w-[75%] sm:w-[40%]">
+      <label className="flex justify-between">
+        {label}:
+        <input
+          className="w-[5%]"
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
 
-      {/* for displaying slider */}
-      <div className="mb-4">
-        <h3 className="text-lg font-semibold mb-2">Password Length</h3>
-        <div className="flex items-center">
-          <p className="mr-2">{passwordLength}</p>
+  return (
+    <div class="bg-gray-300  flex flex-col justify-center items-center     p-4  shadow-md">
+      <div className="min-h-screen w-[97%]  sm:w-[67%] rounded-lg bg-white  flex flex-col justify-center items-center ">
+        {/* for displaying animate from lottie */}
+        <img src={icon} className="sm:w-[18%] w-[45%] " />
+        <h1 className="text-3xl font-bold mb-4">Password Generator</h1>
+
+        {/* for displaying slider */}
+
+        <div className="flex">
+          <h3 className="text-lg font-semibold mb-2">Password Length :</h3>
+          <p className="ml-3 text-lg ">{passwordLength}</p>
+        </div>
+
+        {/* for displaying slider */}
+        <div className=" mb-4 w-[75%] sm:w-[40%]">
           <input
             type="range"
             min={5}
             max={30}
             value={passwordLength}
             onChange={(event) => setPasswordLength(event.currentTarget.value)}
-            className="slider"
+            className="slider w-[99%] "
           />
         </div>
-      </div>
-      {/* for displaying Include Uppercase: */}
-      <div className="mb-4">
-        <label className="flex items-center">
-          Include Uppercase:
-          <input
-            type="checkbox"
-            checked={includeUppercase}
-            onChange={() => setIncludeUppercase(!includeUppercase)}
-            className="ml-2"
-          />
-        </label>
-      </div>
-      {/*for displaying Include Lowercase:  */}
-      <div className="mb-4">
-        <label className="flex items-center">
-          Include Lowercase:
-          <input
-            type="checkbox"
-            checked={includeLowercase}
-            onChange={() => setIncludeLowercase(!includeLowercase)}
-            className="ml-2"
-          />
-        </label>
-      </div>
-      {/* for displaying Include Numbers: */}
-      <div className="mb-4">
-        <label className="flex items-center">
-          Include Numbers:
-          <input
-            type="checkbox"
-            checked={includeNumbers}
-            onChange={() => setIncludeNumbers(!includeNumbers)}
-            className="ml-2"
-          />
-        </label>
-      </div>
-      {/*for displaying  Include Special Characters:  */}
-      <div className="mb-4">
-        <label className="flex items-center">
-          Include Special Characters:
-          <input
-            type="checkbox"
-            checked={includeSpecialChars}
-            onChange={() => setIncludeSpecialChars(!includeSpecialChars)}
-            className="ml-2"
-          />
-        </label>
-      </div>
-      {/* for displaying "Generate Password" button and its function  */}
-      <button
-        onClick={generatePassword}
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-      >
-        Generate Password
-      </button>
-      {/* for generating Password */}
-      <div className="mt-4">
-        <h2 className="text-xl font-semibold ml-3 sm:ml-0">Your Password:</h2>
-        
-        <input
-          type="text"
-          className="text-lg border border-gray-300 p-2 rounded-md sm:ml-0  ml-3  w-[70%]"
-          value={password}
-          readOnly
+
+        {/* for displaying Include Uppercase: */}
+        <CheckboxOption
+          label="Include Uppercase"
+          checked={includeUppercase}
+          onChange={() => setIncludeUppercase(!includeUppercase)}
         />
 
-        {/* for displaying "Copy" button and its function  and checking condition */}
+        {/*for displaying Include Lowercase:  */}
+        <CheckboxOption
+          label="Include Lowercase"
+          checked={includeLowercase}
+          onChange={() => setIncludeLowercase(!includeLowercase)}
+        />
+
+        {/* for displaying Include Numbers: */}
+        <CheckboxOption
+          label="Include Numbers"
+          checked={includeNumbers}
+          onChange={() => setIncludeNumbers(!includeNumbers)}
+        />
+
+        {/*for displaying  Include Special Characters:  */}
+        <CheckboxOption
+          label="Include Special Characters"
+          checked={includeSpecialChars}
+          onChange={() => setIncludeSpecialChars(!includeSpecialChars)}
+        />
+
+        {/* for displaying "Generate Password" button and its function  */}
         <button
-          onClick={copyToClipboard}
-          className={`${
-            copied ? "bg-green-700" : "bg-green-500"
-          } text-white py-2 px-4 ml-3 rounded hover-bg-green-700 mt-2`}
+          onClick={generatePassword}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
         >
-          {copied ? "Copied" : "Copy"}
+          Generate Password
         </button>
+        {/* for generating Password */}
+        <div className="mt-4  ">
+          <h2 className="text-xl font-semibold ml-3">Your Password:</h2>
+          <div className="flex justify-around">
+            <input
+              type="text"
+              className="text-lg border border-gray-400 p-2 rounded-md sm:ml-0   w-[70%]"
+              value={password}
+              readOnly
+            />
 
+            {/* for displaying "Copy" button and its function  and checking condition */}
+            <button
+              onClick={copyToClipboard}
+              className={`${
+                copied ? "bg-green-700" : "bg-green-500"
+              } text-white py-2 px-4  rounded hover-bg-green-700 `}
+            >
+              {copied ? "Copied" : "Copy"}
+            </button>
+          </div>
 
-        {/* for displaying "Strong,Medium,Weak,Too Short" with colour */}
-        <div
-          className={`text-sm ml-3 sm:ml-0 mt-2 ${
-            passwordStrength === "Strong"
-              ? "text-green-500"
-              : passwordStrength === "Medium"
-              ? "text-orange-400"
-              : passwordStrength === "Weak"
-              ? "text-red-500"
-              : passwordStrength === "Too Short"
-              ? "text-red-500"
-              : "text-gray-600"
-          }`}
-        >
-          {passwordStrength}
+          {/* for displaying "Strong,Medium,Weak,Too Short" with colour */}
+          <div
+            className={`text-sm ml-3  mt-2 ${
+              passwordStrength === "Strong"
+                ? "text-green-500"
+                : passwordStrength === "Medium"
+                ? "text-orange-400"
+                : passwordStrength === "Weak"
+                ? "text-red-500"
+                : passwordStrength === "Too Short"
+                ? "text-red-500"
+                : "text-gray-600"
+            }`}
+          >
+            {passwordStrength}
+          </div>
         </div>
       </div>
     </div>
